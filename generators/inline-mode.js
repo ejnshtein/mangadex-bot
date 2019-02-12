4714c8021b4ad24818fb6c9cc690186d4fa056ce
Okabe Rintaro <ejnshtein@gmail.com>
2019-02-12 13:40:01 +0200

    fixed manga search

--- generators/inline-mode.js
+++
@@ -1,9 +0,0 @@
-const { search } = require('mangadex-api').default
-
-module.exports.inlineMode = (searchSegment, query, params = {}) => {
-  params = {
-    page: 1,
-    ...params
-  }
-  return search(query, searchSegment, params)
-}
