dce60c9792c4dc44b3e498fbab46e234267f9fa7
Okabe Rintaro <ejnshtein@gmail.com>
2019-03-14 17:38:11 +0200

    hot fix

---
+++ generators/chapter-view.js
@@ -0,0 +1,87 @@
+const Mangadex = require('mangadex-api').default
+const { templates, getList } = require('../lib')
+const collection = require('../core/database')
+
+const client = new Mangadex({
+  shareChapterCache: true,
+  shareMangaCache: true
+})
+
+module.exports = async (chapterId, offset = 0, history = 'p=1:o=0', list) => {
+  let chapter = await client.getChapter(chapterId)
+  const manga = await client.getManga(chapter.manga_id, false)
+  // console.log(chapter)
+  try {
+    var chapterExists = await collection('chapters').findOne({ id: chapter.id }).exec()
+  } catch (e) {
+    return {
+      chapter,
+      manga,
+      text: `Oops, something went wrong...`,
+      extra: {
+        parse_mode: 'HTML'
+      }
+    }
+  }
+  if (chapterExists) {
+    chapter.telegraph = chapterExists.telegraph
+    const keyboard = [
+      [
+        {
+          text: 'Instant View',
+          url: chapter.telegraph
+        },
+        {
+          text: 'Share chapter',
+          switch_inline_query: `chapter:${chapterId}`
+        }
+      ],
+      [
+        {
+          text: 'Chapter list',
+          callback_data: `${list ? `list=${list}:` : ''}chapterlist=${chapter.lang_code}:id=${chapter.manga_id}:offset=${offset}${list ? '' : `:${history}`}`
+        },
+        {
+          text: 'Manga description',
+          callback_data: `${list ? `list=${list}:` : ''}manga=${chapter.manga_id}${list ? '' : `:${history}`}`
+        }
+      ],
+      manga.manga.links['mal'] ? [
+        {
+          text: 'Track reading on MAL',
+          url: `https://myanimelist.net/manga/${manga.manga.links['mal']}`
+        }
+      ] : undefined
+    ].filter(Boolean)
+    const messageText = templates.manga.chapter(chapter, manga, undefined, list ? `<b>List:</b> ${getList(list.match(/([a-z]+)/i)[1])}` : '')
+    return {
+      chapter,
+      manga,
+      text: messageText,
+      extra: {
+        parse_mode: 'HTML',
+        reply_markup: {
+          inline_keyboard: keyboard
+        }
+      }
+    }
+  } else {
+    return {
+      chapter,
+      manga,
+      text: `${templates.chapter.formatChapter(chapter)} isn't cached yet. Do you want to cache it?`,
+      extra: {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: 'Sure.',
+                callback_data: `${list ? `list=${list}:` : ''}chapter=${chapter.id}:read=false:copy=false:offset=${offset}${list ? '' : `:${history}`}`
+              }
+            ]
+          ]
+        }
+      }
+    }
+  }
+}
